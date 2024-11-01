df041105af7bb590f92b141c52975de3c5f6e89e
Gregor Noczinski <gregor@noczinski.eu>
2024-11-01 21:17:41 +0100

    Fix tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,31 +1,39 @@
+import {KVNamespace} from '@cloudflare/workers-types';
 import {Environment} from './common';
 import {handler} from './index';
 
+const testKv: KVNamespace = {
+   put() {
+      throw `Not implemented!`;
+   },
+   get() {
+      throw `Not implemented!`;
+   },
+   list() {
+      throw `Not implemented!`;
+   },
+   delete() {
+      throw `Not implemented!`;
+   },
+   // @ts-ignore
+   getWithMetadataetadata() {
+      throw `Not implemented!`;
+   },
+};
+
 const testEnvironment: Environment = {
-    GITHUB_ACCESS_USER: 'testGithubAccessUser',
-    GITHUB_ACCESS_TOKEN: 'testGithubAccessToken',
-    KV: {
-        put() {
-            throw `Not implemented!`;
-        },
-        get() {
-            throw `Not implemented!`;
-        },
-        list() {
-            throw `Not implemented!`;
-        },
-        delete() {
-            throw `Not implemented!`;
-        },
-        // @ts-ignore
-        getWithMetadataetadata() {
-            throw `Not implemented!`;
-        },
-    },
+   GITHUB_ACCESS_USER: 'testGithubAccessUser',
+   GITHUB_ACCESS_TOKEN: 'testGithubAccessToken',
+   GITHUB_ORGANIZATION: 'testGithubOrganization',
+   GITHUB_REPOSITORY: 'testGithubRepository',
+   KV: testKv,
+   ACCOUNT_ID: 'testAccountId',
+   PROJECT_NAME: 'testProjectName',
+   API_TOKEN: 'testApiToken',
 };
 
 test('GET /', async () => {
-    const result = await handler.fetch(new Request('http://falcon/latest/', {method: 'GET'}), testEnvironment);
-    expect(result.status).toBe(301);
-    expect(result.headers.get('Location')).toBe('http://falcon/');
+   const result = await handler.fetch(new Request('http://falcon/latest/', { method: 'GET' }), testEnvironment);
+   expect(result.status).toBe(301);
+   expect(result.headers.get('Location')).toBe('http://falcon/');
 });
