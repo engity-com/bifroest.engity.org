1dffb39157be466c117143cfef31a857f0d21370
Gregor Noczinski <gregor@noczinski.eu>
2024-11-03 20:06:14 +0100

    Improve cache handling.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { App } from './app';
+import { Environment } from './common';
 
-export const handler = new App();
+const app = new App();
 
+export const handler = {
+   async fetch(request: Request, env: Environment, ctx: ExecutionContext): Promise<Response> {
+      return await app.fetch(request, env, ctx);
+   },
+
+   async scheduled(scheduledController: ScheduledController, env: Environment, ctx: ExecutionContext): Promise<void> {
+      return await app.scheduled(scheduledController, env, ctx);
+   },
+};
+
+// noinspection JSUnusedGlobalSymbols
 export default handler;
